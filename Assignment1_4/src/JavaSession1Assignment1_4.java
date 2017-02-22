6cae64187c9c7ae364cd480ca93335da3b6284e3
Sandeep Hegde <sandeep.hegde123@gmail.com>
2017-02-23 01:22:42 +0530

    Java Session 1 Assignment 1.4
    Session 1 Assignment 1.4 of Core Java Course

---
+++ Assignment1_4/src/JavaSession1Assignment1_4.java
@@ -0,0 +1,29 @@
+//Print the characters corresponding to ASCII codes from 65 to 90 in a tabular format like 
+//65-A
+//66-B
+
+public class JavaSession1Assignment1_4 {
+
+	
+public static void main (String[] args) 
+     
+     {
+    	 
+    	    char a ;
+
+ 		    for (int i = 65 ; i <= 90 ; i++)
+ 			
+ 	 {
+ 			
+ 			a= (char)i;
+ 			
+ 			System.out.println(i+ " - "+a);
+ 			
+ 			
+ 	 }
+ 		    
+    	 
+     }
+	
+	
+}
